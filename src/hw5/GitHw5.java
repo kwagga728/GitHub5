d4cc6563312478901cad578bb1ff4f236059a548
brett <brett@DonLish>
2025-01-30 09:12:47 -0500

    CreateRpository

---
+++ src/hw5/GitHw5.java
@@ -0,0 +1,49 @@
+package hw5;
+
+public class GitHw5 {
+	
+	public static void main(String[] args) {
+		String s1 = "JAVA IS FUN";
+		
+		String s2 = s1.toLowerCase();
+		
+		System.out.println("s1: " + s1);
+		System.out.println("s2: " + s2);
+		
+		/**
+
+		1. What is the methodName?
+
+			toLowerCase()
+		 
+		2. How many parameters does the method take?
+
+			Zero
+			 
+
+		3. If it takes parameter, then what are the data types?
+
+		 	N/A
+
+		4. What is the return type of the method?
+
+		 	String
+
+		5. What is the purpose of this method? What does it do??
+
+		 	This method converts the value of string to lowercase
+		 
+
+		*/
+		
+		System.out.println("************************");
+		
+		String s3 = "today's topic is fun";
+		String s4 = s3.toUpperCase();
+		
+		System.out.println("s3: " + s3);
+		System.out.println("s4: " + s4);
+	}
+
+
+}
